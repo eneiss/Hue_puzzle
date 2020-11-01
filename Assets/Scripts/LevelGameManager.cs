c0eea43877d915bd43a1bb5eee0ea0f96b8e04d9
Emma <emmaneiss2@gmail.com>
2020-11-01 15:44:02 +0100

    Initial commit

---
+++ Assets/Scripts/LevelGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+// using System.Diagnostics;
+using UnityEngine;
+
+public class LevelGameManager : MonoBehaviour
+{
+
+    GameObject[] tiles;
+    List<(int dx, int dy)> neighbours;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("Tile");
+        neighbours = new List<(int dx, int dy)>
+        {
+            (-1, -1), (-1, 1), (1, -1), (1, 1),     // diag
+            (0, 1), (0, -1), (1, 0), (-1, 0)
+        };
+    }
+
+    public void InvertTiles(int x, int y)
+    {
+        foreach ((int dxVar, int dyVar) in neighbours)
+        {
+            Debug.Log("dx : " + dxVar + ", dy : " + dyVar);
+        }
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+// using System.Diagnostics;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+
+    LevelGameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = (LevelGameManager) GameObject.FindWithTag("LGM").GetComponent(typeof(LevelGameManager));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (gameObject.GetComponent<BoxCollider2D>().OverlapPoint(Camera.main.ScreenToWorldPoint(touch.position)))
+            {
+                Click();
+            }
+        }
+        if (Input.GetMouseButtonUp(0) && gameObject.GetComponent<BoxCollider2D>().OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+        {
+            Click();
+        }
+    }
+
+    void Click()
+    {
+        Debug.Log("Click");
+        InvertColor();
+        gameManager.InvertTiles(0, 0);
+    }
+
+    public void InvertColor()
+    {
+        Debug.Log("Inverting color");
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.color = new Color(1f - renderer.color.r, 1f - renderer.color.g, 1f - renderer.color.b, 1f);
+    }
+}
